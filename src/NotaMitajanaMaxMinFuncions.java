2a281f65598f2e8a6994a84123f8b066ba5f6c2f
Raul <rauljouman@gmail.com>
2024-02-02 17:00:09 +0100

    Subo los dos programas arreglado del PrediccioPersona y hecho el de MinMaxMitjana

---
+++ src/NotaMitajanaMaxMinFuncions.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+public class NotaMitajanaMaxMinFuncions {
+
+    private double[] notes = new double[5];
+    private double notaMax;
+    private double notaMin;
+    private double notaMitjana;
+
+    public static void main  (String []args) {
+        NotaMitajanaMaxMinFuncions programa = new NotaMitajanaMaxMinFuncions();
+        programa.inici();
+    }
+
+    public void inici(){
+        mostraMenu();
+        llegirDades();
+    }
+
+    public void mostraMenu() {
+        System.out.println("Hola, diga'm 5 notes para posar a l'array.");
+        System.out.println("Amb aquestes notes calculare la nota màxima, mínima i la mitjana.");
+    }
+
+    public void llegirDades(){
+        elementsArray(notes);
+        notaMax = notes[0];
+        notaMin = notes[0];
+        notaMitjana = notes[0];
+        calculaMax(notes);
+        calculaMin(notes);
+        calculaMitjana(notes);
+        llegirResultat(notaMax, notaMin, notaMitjana);
+    }
+
+    public double[] elementsArray (double[] notes) {
+        //Afegeix notes notes
+        Scanner sc = new Scanner (System.in);
+        boolean pasa = false;
+        while (!pasa) {
+            for (int i = 0; i < notes.length; i++) {
+                if (sc.hasNextDouble()) {
+                    notes[i] = sc.nextDouble();
+                    pasa = true;
+                } else {
+                    System.out.println("No vàlid, repeteix un altre cop");
+                    sc.next();
+                    i--;
+                }
+            }
+        }
+        return notes;
+    }
+
+    public double calculaMax(double[] notes) {
+        for (int i = 0; i < notes.length; i++) {
+            if (notaMax < notes[i]) {
+                notaMax = notes[i];
+                System.out.println("Nova data guardada");
+            }
+        }
+        return notaMax;
+    }
+
+    public double calculaMin(double[] notes) {
+        for (int i = 0; i < notes.length; i++) {
+            if (notaMin > notes[i]) {
+                notaMin = notes[i];
+                System.out.println("Nova data guardada");
+            }
+        }
+        return notaMin;
+    }
+
+    public double calculaMitjana(double[] notes) {
+        int numNotas = 0;
+        for (int i = 0; i < notes.length; i++) {
+            notaMitjana+= notes[i];
+            numNotas++;
+        }
+        notaMitjana = notaMitjana / numNotas;
+        return notaMitjana;
+    }
+
+    public void llegirResultat(double notaMax, double notaMin, double notaMitjana){
+        System.out.println("La nota més alta es... " + notaMax);
+        System.out.println("La nota més baixa ha sigut... " + notaMin);
+        System.out.println("La nota mitjana ha sigut... " + notaMitjana);
+    }
+
+}
---
+++ src/PrediccioPersona.java
@@ -0,0 +1,216 @@
+
+    import java.util.Scanner;
+    public class PrediccioPersona {
+
+        private Scanner sc = new Scanner(System.in);
+        private String mesNaixement = "";
+
+        public static void main(String[] args) {
+            PrediccioPersona programa = new PrediccioPersona();
+            programa.inici();
+        }
+
+        public void inici() {
+            preguntaYResposta();
+            triaNom();
+            triaEdat(mesNaixement);
+        }
+
+        public void preguntaYResposta() {
+            //Mostra missatge per escullir mes i amb la resposta
+            System.out.print("Escriu el mes en què vas nèixer.");
+            System.out.print("\n");
+            System.out.print("Gener\tFebrer\tMarc\n");
+            System.out.print("Abril\tMaig\tJuny\n");
+            System.out.print("Juliol\tAgost\tSetembre\n");
+            System.out.print("Octubre\tNovembre\tDesembre");
+            System.out.print("\n\n");
+            mesNaixement = sc.nextLine();
+            seleccioEquip(mesNaixement);
+            seleccioLlenguante(mesNaixement);
+        }
+
+        public String triaNom() {
+            //Escull Nom
+            String nomEscullit = " ";
+            boolean correcte = false;
+            System.out.print("Com et dius?\n");
+            while (!correcte) {
+                if (!sc.hasNextDouble()){
+                    nomEscullit = sc.nextLine();
+                    correcte = true;
+                }
+                else {
+                    System.out.println("No es un nom, prova un altre cop!");
+                    sc.nextLine();
+                }
+            }
+            return nomEscullit;
+        }
+
+        public int triaEdat(String mesNaixement){
+            //Escull anys
+            System.out.print("\n");
+            System.out.print("Per acabar, quants anys tens?\n");
+            int edat = 0;
+            boolean correcte = false;
+            while (!correcte) {
+                if (sc.hasNextInt()){
+                    edat = sc.nextInt();
+                    correcte = true;
+                }
+                else {
+                    System.out.println("No m'has posat un edat!");
+                    sc.nextLine();
+                }
+
+            }
+            seleccioNumSort(edat, mesNaixement);
+            return edat;
+        }
+
+        public String seleccioEquip(String mesNaixement){
+            String equipFutbol = "";
+            if (mesNaixement.equalsIgnoreCase("Gener")) {
+                equipFutbol = "Bayern de Munich";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Febrer")){
+                equipFutbol = "Atlético de Madrid";
+            }
+            else if (mesNaixement.equalsIgnoreCase("marc")){
+                equipFutbol = "Chelsea";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Abril")){
+                equipFutbol = "Manchester city";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Maig")){
+                equipFutbol = "Liverpool";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Juny")){
+                equipFutbol = "Real Madrid";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Juliol")){
+                equipFutbol = "BVB";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Agost")){
+                equipFutbol = "FCB";
+
+            }
+            else if (mesNaixement.equalsIgnoreCase("Setembre")){
+                equipFutbol = "Juve";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Octubre")){
+                equipFutbol = "PSG";
+
+            }
+            else if (mesNaixement.equalsIgnoreCase("Novembre")){
+                equipFutbol = "Sevilla CF";
+            }
+            else if (mesNaixement.equalsIgnoreCase("Desembre")){
+                equipFutbol = "Manchester United";
+            }
+            else {
+                equipFutbol = "no tinc equip de fútbol perquè m'he equivocat escrivint el meu mes d'aniversari";
+            }
+            return equipFutbol;
+        }
+
+        public String seleccioLlenguante(String mesNaixement) {
+            String llenguatgeProgramacio = "";
+            if (mesNaixement.equalsIgnoreCase("Gener")) {
+                llenguatgeProgramacio = "C++";
+            } else if (mesNaixement.equalsIgnoreCase("Febrer")) {
+                llenguatgeProgramacio = "Python";
+            } else if (mesNaixement.equalsIgnoreCase("marc")) {
+                llenguatgeProgramacio = "JavaScript";
+            } else if (mesNaixement.equalsIgnoreCase("Abril")) {
+                llenguatgeProgramacio = "Node.js";
+            } else if (mesNaixement.equalsIgnoreCase("Maig")) {
+                llenguatgeProgramacio = "React";
+            } else if (mesNaixement.equalsIgnoreCase("Juny")) {
+                llenguatgeProgramacio = "C#";
+            } else if (mesNaixement.equalsIgnoreCase("Juliol")) {
+                llenguatgeProgramacio = "Swift";
+            } else if (mesNaixement.equalsIgnoreCase("Agost")) {
+                llenguatgeProgramacio = "Java";
+            } else if (mesNaixement.equalsIgnoreCase("Setembre")) {
+                llenguatgeProgramacio = "PHP";
+            } else if (mesNaixement.equalsIgnoreCase("Octubre")) {
+                llenguatgeProgramacio = "R";
+            } else if (mesNaixement.equalsIgnoreCase("Novembre")) {
+                llenguatgeProgramacio = "Go";
+            } else if (mesNaixement.equalsIgnoreCase("Desembre")) {
+                llenguatgeProgramacio = "Ruby";
+            } else {
+                llenguatgeProgramacio = "no tinc llenguatge de programació preferit perquè m'he equivocat escrivint el meu mes d'aniversari";
+                System.out.print("No he entès el que m'has dit!!!!!!!!!!!!\n\n");
+            }
+            return llenguatgeProgramacio;
+        }
+
+        public double seleccioNumSort(int edat, String mesNaixement) {
+            double numSort = 0;
+            if (mesNaixement.equalsIgnoreCase("Gener")) {
+                numSort = edat * 3/2;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Febrer")){
+                numSort = edat - 4;
+            }
+            else if (mesNaixement.equalsIgnoreCase("marc")){
+                numSort = edat / 2;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Abril")){
+                numSort = edat % 4;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Maig")){
+                numSort = edat * 3/6;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Juny")){
+                numSort = edat + edat;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Juliol")){
+                numSort = edat * edat / 5;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Agost")){
+                numSort = edat * 1/2 * edat;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Setembre")){
+                numSort = edat / edat;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Octubre")){
+                numSort = edat * 4 / 3;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Novembre")){
+                numSort = edat % 2 + 23;
+            }
+            else if (mesNaixement.equalsIgnoreCase("Desembre")){
+                numSort = (edat + edat) / 2;
+            }
+            else {
+                numSort = 0;
+                System.out.print("No he entès el que m'has dit!!!!!!!!!!!!\n\n");
+            }
+            return numSort;
+        }
+
+        public void mostraMissatgeFinal(String nomEscullit, double numSort, String equipFutbol, String llenguatgeProgramacio) {
+            //Missatge final amb les variables
+            System.out.print("------------------------------------------------\n");
+
+            System.out.print("Un plaer conèixer-te " + nomEscullit + ". Amb les dades que m'has donat, mira què he arribat a saber de tu... \n");
+            System.out.print("\n\n");
+
+            System.out.print(">>> El teu número de la sort és el " + numSort + "!");
+            System.out.print("\n\n");
+
+            System.out.print(">>> El teu equip de fútbol preferit, encara que no ho diguis en veu alta, és el " + equipFutbol + ".");
+            System.out.print("\n\n");
+
+            System.out.print(">>> El teu llenguatge de programació més estimat serà " + llenguatgeProgramacio + "!");
+            System.out.print("\n\n");
+
+            System.out.print("Torna quan vulguis!\n");
+            System.out.print("------------------------------------------------\n\n\n");
+
+        }
+}
