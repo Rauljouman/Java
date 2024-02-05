f8b48de0b8bd3bd97b5847a14d8566b45597a48b
Raul <rauljouman@gmail.com>
2024-02-05 20:41:12 +0100

    prog mod: registre notes i càlcul d'àrees

---
+++ src/UF2/calculAreesModular/TriangleOperacio.java
@@ -0,0 +1,12 @@
+package UF2.calculAreesModular;
+
+public class TriangleOperacio {
+    public double area = 0;
+
+    public double triangle() {
+        double b = 23.0;
+        double h = 8.0;
+        area = (b * h) / 2;
+        return area;
+    }
+}
